dbeeef68c64f684b6c53566a1f7b4f09aeb29b91
zzm <512222607@qq.com>
2018-03-28 13:46:30 +0800

    测试提交

--- chapter1/src/main/java/com/zhangzm/concurrency/module2/exaple_bank/Bank.java
+++ chapter1/src/main/java/com/zhangzm/concurrency/module2/exaple_bank/Bank.java
@@ -9,10 +9,8 @@ public class Bank {
 	public static void main(String[] args) {
 		TicketWindow window1 = new TicketWindow("一号柜台");
 		window1.start();
-
 		TicketWindow window2 = new TicketWindow("二号柜台");
 		window2.start();
-
 		TicketWindow window3 = new TicketWindow("三号柜台");
 		window3.start();
 	}
